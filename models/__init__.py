96ce55217c4360b001bab598cb4beafc41dd3f1c
June.L <blue_mind88@hotmail.com>
2021-03-26 16:33:23 +0900

    add vanilla_3d_2

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from junet.models import res_unet_3d
 from junet.models import vanilla_unet_3d
 from junet.models import xception_unet_3d
 from junet.models import vanilla_unet
+from junet.models import vanilla_unet_3d_2
