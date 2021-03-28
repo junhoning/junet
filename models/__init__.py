5f7633f339d20590226b6c8350a8d1543e51ec4f
June.L <blue_mind88@hotmail.com>
2021-03-28 15:35:33 +0900

    modify model names

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,6 @@
-from junet.models import *
+from junet.models import dense_unet_3d
+from junet.models import res_unet_3d
+from junet.models import vanilla_unet
+from junet.models import vanilla_unet_3d
+from junet.models import vanilla_unet_3d_2
+from junet.models import xception_unet_3d
